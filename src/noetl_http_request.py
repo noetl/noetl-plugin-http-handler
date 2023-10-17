7b3fa7036e6139a2c2bc8e34e3d2a8c533285b4d
akuksin <4165556+akuksin@users.noreply.github.com>
2023-10-16 23:08:21 -0700

    http request service

---
+++ src/noetl_http_request.py
@@ -0,0 +1,41 @@
+import asyncio
+from nats.aio.client import Client as NATS
+import httpx
+import json
+
+async def run(loop):
+    nc = NATS()
+    await nc.connect(servers=["nats://nats-server:4222"], loop=loop)
+
+    # Register with NATS
+    registration_payload = {
+        "name": "http-request",
+        "source_path": "https://github.com/noetl/noetl-plugin-http-handler.git",
+        "image_path": "registry/noetl-plugin-request-image:latest",
+        "schema": {
+            "input": {"type": "string", "description": "URL to fetch"},
+            "output": {"type": "string", "description": "HTTP response"}
+        },
+        "run_option": "service"  # or "job"
+    }
+    await nc.publish("registration", json.dumps(registration_payload).encode())
+
+    # Subscribe to commands
+    async def handle_command(msg):
+        url = msg.data.decode()
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(url)
+        result_payload = {
+            "result": resp.text
+        }
+        await nc.publish("events", json.dumps(result_payload).encode())
+
+    await nc.subscribe("commands", cb=handle_command)
+
+    # Keep running
+    await asyncio.sleep(3600)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(loop))
+    loop.close()
